c30f1ed92a4cdedc5e6e92f326d14a79a4f3c7ec
Admire Musimwa <addymhoti@gmail.com>
2017-08-19 14:03:56 +0200

    Included links and scripts for Bootstrap in all the pages

--- public/contact.php
+++ public/contact.php
@@ -8,7 +8,10 @@ require __DIR__ . '/../app/src/send-email.php';
 	<head>
 		<meta charset="utf-8">
 		<title><?php echo $json_data['user']['username']; ?>'s badges</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<link rel="stylesheet" href="../app/css/main.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</head>
 	<body>
 		<?php
--- public/skills.php
+++ public/skills.php
@@ -3,7 +3,10 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Admire Musimwa</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<link rel="stylesheet" href="../app/css/main.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</head>
 	<body>
 		<?php
